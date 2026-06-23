041037ab6dc8677f87484fce828581652ceed069
Monish <bmonish2003@gmail.com>
2026-06-23 13:35:31 +0530

    data types

---
+++ DataTypes.py
@@ -0,0 +1,21 @@
+#Data Types 
+
+#Text Types
+# string - str
+# a= "Akash" , 'Aaksh' (you can provide single qutation or double quatiton)
+#Numerical Types
+# a= 123 - Integer (int)
+# a= 23.78 Float (float)
+# a = i + 3j complex (complex)
+#Boolen Types
+# a= True , False (bool)
+# Squence Types
+# a = ["item1","item2","item3"] : [1,23,4,5,5] - (list)
+# a = ("item1","item2") : (2,4,6,79,0) - (tuple)
+# Mapping Types
+# a = {"name":"akash","age":27,"city":"Bangalore"} - (dict)
+# Set Types
+# a = {3,56,7,86,5,67}  - (set)
+# a = frozenset{3,45,6,77,88} - (frozenset)
+ 
+
--- python.py
+++ python.py
@@ -1,1 +1,1 @@
-print("monish ")
+Fruit=[]
